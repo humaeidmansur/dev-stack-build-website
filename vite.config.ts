3ec34c42713def36f317cdef8c3ad6a97a53080f
Humaeid Bin Mansur <humaeidmansur@gmail.com>
2026-09-23 21:41:30 +0200

    GitHub deployment issue solved in vite config ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: process.env.NODE_ENV === 'production'
+    ? '/dev-stack-build-website/'
+    : '/',
   plugins: [
     react(),
-    tailwindcss()
-  
+    tailwindcss(),
   ],
 })
